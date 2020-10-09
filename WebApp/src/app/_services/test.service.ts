9e34d38443d8c7cd3d4d05099b9f56671c689fef
schmelto <tom.schmelzer@web.de>
2020-10-10 00:03:37 +0200

    test

--- WebApp/src/app/_services/test.service.ts
+++ WebApp/src/app/_services/test.service.ts
@@ -6,13 +6,19 @@ import { Injectable } from '@angular/core';
 })
 export class TestService {
 
-
-  readonly BaseURI = 'vortex.cubuzz.de:8500/api/v1';
+  totalAngularPackages: any;
+  readonly BaseURI = "vortex.cubuzz.de:8500/api/v1";
 
   constructor(private http: HttpClient) { }
 
   public getTest() {
     return this.http.get(this.BaseURI);
+    
   }
-
+  public getTest2(){
+    this.http.get<any>(this.BaseURI).subscribe(data => {
+          this.totalAngularPackages = data.total;
+      })     
+      return this.totalAngularPackages; 
+    }
 }
--- WebApp/src/app/home/home.component.ts
+++ WebApp/src/app/home/home.component.ts
@@ -13,10 +13,22 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     console.log('Test');
     console.log(this.service.getTest());
+
+    console.log('Test2');
+    var Http = new XMLHttpRequest();
+    var url='http://vortex.cubuzz.de:8500/api/v1/';
+    Http.open("GET", url);
+    Http.send();
+  
+    Http.onreadystatechange = (e) => {
+      console.log(Http.responseText)
+    }
   }
 
   test() {
     console.log('Test');
-    console.log(this.service.getTest());
+    console.log(this.service.getTest2());
   }
+
+
 }
