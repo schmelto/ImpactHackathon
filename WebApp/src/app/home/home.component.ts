3735299d45b350bfeb15ca32675e8ccedeba0f89
schmelto <tom.schmelzer@web.de>
2020-10-10 02:50:58 +0200

    api call semi functional

--- WebApp/src/app/home/home.component.ts
+++ WebApp/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
+import { localizedString } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { loadavg } from 'os';
 import { TestService } from '../_services/test.service';
 
 @Component({
@@ -8,27 +10,55 @@ import { TestService } from '../_services/test.service';
 })
 export class HomeComponent implements OnInit {
 
+  products;
+
   constructor(public service: TestService) { }
 
-  ngOnInit(): void {
-    console.log('Test');
-    console.log(this.service.getTest());
-
-    console.log('Test2');
-    var Http = new XMLHttpRequest();
-    var url='http://vortex.cubuzz.de:8500/api/v1/';
-    Http.open("GET", url);
-    Http.send();
-  
-    Http.onreadystatechange = (e) => {
-      console.log(Http.responseText)
-    }
+  ngOnInit() {
+    this.loadData();
+    
   }
 
-  test() {
-    console.log('Test');
-    console.log(this.service.getTest2());
+  loadData() {
+    this.service.getTest().subscribe((data: any)=>{
+      this.products = data;
+      console.log(this.products);
+    });
+
+    console.log(this.products);
   }
 
 
 }
+
+
+//   console.log(this.service.getTest());
+
+  //   this.service.getTest().subscribe((data: any) => {
+  //     console.log("subscribe:" + data);
+  //   });
+
+
+
+
+  //   var Http = new XMLHttpRequest();
+  //   var url='http://vortex.cubuzz.de:8500/api/v1/';
+  //   Http.open("GET", url);
+  //   Http.send();
+  
+  //   Http.onreadystatechange = (e) => {
+  //     console.log(Http.responseText)
+  //   }
+  //   let varia;
+  //   this.service.getTest().subscribe((res) => {
+  //     varia = res;
+  //     console.log("subscribe:" + res);
+  //   });
+  //   console.log(varia);
+
+
+  // }
+
+  // test() {
+    
+  // }
