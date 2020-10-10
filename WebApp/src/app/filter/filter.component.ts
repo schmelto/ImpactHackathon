30e152e9d7fdbaf61b70ae440646832b96f8dacf
Katharina Ehrmann <kath.ehrmann@web.de>
2020-10-10 12:52:46 +0200

    add filters

--- WebApp/src/app/filter/filter.component.ts
+++ WebApp/src/app/filter/filter.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter.component.scss']
 })
 export class FilterComponent implements OnInit {
-
+  noOfRooms = false;
+  socialFacts = false;
+  perfectFlatMates = false;
+  infrastructure = false;
+  //socialFacts = true;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  flatClick(){
+    this.noOfRooms = !this.noOfRooms;
+  }
+  sharedFlatClick(){
+    this.perfectFlatMates = !this.perfectFlatMates;
+  }
+  socialFactsClick(){
+    this.socialFacts = !this.socialFacts;
+  }
+  infrastructureClick(){
+    this.infrastructure = !this.infrastructure;
   }
 
 }
