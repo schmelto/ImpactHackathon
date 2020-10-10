16ec1530bfd5489bb8019c1339c79389828c1d9c
schmelto <tom.schmelzer@web.de>
2020-10-10 11:14:16 +0200

    basic like

--- WebApp/src/app/home/home.component.ts
+++ WebApp/src/app/home/home.component.ts
@@ -11,11 +11,46 @@ export class HomeComponent implements OnInit {
   
 
   constructor(public service: TestService) { }
-
-  data$;
+  index = 0;
+  currentdata$;
+  data$ = [
+    {
+      "title": "Test",
+      "type": "WG",
+      "description": "test1233",
+      "imgurl": "https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80"
+    },
+    {
+      "title": "Test123",
+      "type": "House",
+      "description": "test1231231231233",
+      "imgurl": "https://images.unsplash.com/photo-1501183638710-841dd1904471?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+    },
+    {
+      "title": "Test123",
+      "type": "Penthouse",
+      "description": "test1231231231233",
+      "imgurl": "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+    },
+    {
+      "title": "Test123",
+      "type": "House",
+      "description": "test1231231231233",
+      "imgurl": "https://images.unsplash.com/photo-1572120360610-d971b9d7767c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+    },
+    {
+      "title": "Test123",
+      "type": "House",
+      "description": "test1231231231233",
+      "imgurl": "https://images.unsplash.com/photo-1572120360610-d971b9d7767c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+    }
+    
+  ];
 
   ngOnInit() {
-    this.loadData();
+    // this.loadData();
+    this.currentdata$ = this.data$[this.index];
+    console.log(this.currentdata$)
     
   }
 
@@ -23,6 +58,16 @@ export class HomeComponent implements OnInit {
     this.service.getTest().subscribe((data: any) => {
       this.data$ = data;
     });
+  }
+
+  nextdata() {
+    console.log(this.index);
+    this.index++;
+    this.currentdata$ = this.data$[this.index];
+  }
+
+  test() {
+    console.log("Test");
   }
 
 
