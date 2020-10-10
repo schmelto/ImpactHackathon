49c2f0872cde47224154fddcee0766d8d7d5854d
schmelto <tom.schmelzer@web.de>
2020-10-10 13:40:12 +0200

    call api

--- WebApp/src/app/_services/test.service.ts
+++ WebApp/src/app/_services/test.service.ts
@@ -1,13 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-const options = {responseType: 'text' as 'text'};
-// const options = {responseType: 'json' as 'json'};
+// const options = {responseType: 'text' as 'text'};
+const options = {responseType: 'json' as 'json'};
 @Injectable({
   providedIn: 'root'
 })
 export class TestService {
   
-  readonly BaseURI = "http://vortex.cubuzz.de:8500/api/v1/";
+  readonly BaseURI = "http://vortex.cubuzz.de:8500/api/v1/search/dummy";
 
   constructor(private http: HttpClient) { }
 
--- WebApp/src/app/home/home.component.ts
+++ WebApp/src/app/home/home.component.ts
@@ -16,57 +16,21 @@ export class HomeComponent implements OnInit {
   openDialog(){
     this.dialog.open(DialogElementsExampleDialog, {data: this.currentdata$});
   }
-  index = 0;
+  index = -1;
   currentdata$;
-  data$ = [
-    {
-      "title": "Nice WG in Downtown",
-      "type": "WG",
-      "location": "Musterstraße 42, 74214 Musterstadt",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis posuere morbi leo urna molestie at. Aliquet enim tortor at auctor urna nunc id cursus. Faucibus nisl tincidunt eget nullam non nisi est sit amet. Elit ut aliquam purus sit amet luctus. Lacus laoreet non curabitur gravida arcu ac. Scelerisque eleifend donec pretium vulputate sapien nec. Viverra nam libero justo laoreet sit. Enim sed faucibus turpis in. Vel elit scelerisque mauris pellentesque.",
-      "imgurl": "https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80"
-    },
-    {
-      "title": "Test123",
-      "type": "House",
-      "location": "Musterstraße 42, 74214 Musterstadt",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis posuere morbi leo urna molestie at. Aliquet enim tortor at auctor urna nunc id cursus. Faucibus nisl tincidunt eget nullam non nisi est sit amet. Elit ut aliquam purus sit amet luctus. Lacus laoreet non curabitur gravida arcu ac. Scelerisque eleifend donec pretium vulputate sapien nec. Viverra nam libero justo laoreet sit. Enim sed faucibus turpis in. Vel elit scelerisque mauris pellentesque.",
-      "imgurl": "https://images.unsplash.com/photo-1501183638710-841dd1904471?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-    },
-    {
-      "title": "Test123",
-      "type": "Penthouse",
-      "location": "Musterstraße 42, 74214 Musterstadt",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis posuere morbi leo urna molestie at. Aliquet enim tortor at auctor urna nunc id cursus. Faucibus nisl tincidunt eget nullam non nisi est sit amet. Elit ut aliquam purus sit amet luctus. Lacus laoreet non curabitur gravida arcu ac. Scelerisque eleifend donec pretium vulputate sapien nec. Viverra nam libero justo laoreet sit. Enim sed faucibus turpis in. Vel elit scelerisque mauris pellentesque.",
-      "imgurl": "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-    },
-    {
-      "title": "Test123",
-      "type": "House",
-      "location": "Musterstraße 42, 74214 Musterstadt",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis posuere morbi leo urna molestie at. Aliquet enim tortor at auctor urna nunc id cursus. Faucibus nisl tincidunt eget nullam non nisi est sit amet. Elit ut aliquam purus sit amet luctus. Lacus laoreet non curabitur gravida arcu ac. Scelerisque eleifend donec pretium vulputate sapien nec. Viverra nam libero justo laoreet sit. Enim sed faucibus turpis in. Vel elit scelerisque mauris pellentesque.",
-      "imgurl": "https://images.unsplash.com/photo-1572120360610-d971b9d7767c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-    },
-    {
-      "title": "Lorem ipsum",
-      "type": "House",
-      "location": "Musterstraße 42, 74214 Musterstadt",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis posuere morbi leo urna molestie at. Aliquet enim tortor at auctor urna nunc id cursus. Faucibus nisl tincidunt eget nullam non nisi est sit amet. Elit ut aliquam purus sit amet luctus. Lacus laoreet non curabitur gravida arcu ac. Scelerisque eleifend donec pretium vulputate sapien nec. Viverra nam libero justo laoreet sit. Enim sed faucibus turpis in. Vel elit scelerisque mauris pellentesque.",
-      "imgurl": "https://images.unsplash.com/photo-1572120360610-d971b9d7767c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-    }
-    
-  ];
+  data$ = [];
 
   ngOnInit() {
-    // this.loadData();
+    this.loadData();
     this.currentdata$ = this.data$[this.index];
-    console.log(this.currentdata$)
+    
     
   }
 
    loadData() {
     this.service.getTest().subscribe((data: any) => {
       this.data$ = data;
+      console.log(data)
     });
   }
 
